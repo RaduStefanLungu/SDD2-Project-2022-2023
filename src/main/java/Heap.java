283d96511ef926ceb1756646154ada2552da0f6a
M3triXxX <strongheartgamingro@gmail.com>
2023-03-07 16:44:59 +0100

    -> Heap adding done. -> TODO : Add removing from heap

--- src/main/java/Heap.java
+++ src/main/java/Heap.java
@@ -1,12 +1,10 @@
 import java.util.ArrayList;
 
 public class Heap {
-    // representation arborescente de Heap
-
-    private ArrayList<Integer> heap;
+    private ArrayList<HeapNode> heap;
 
     public Heap(){
-        this.heap = new ArrayList<Integer>();
+        this.heap = new ArrayList<HeapNode>();
     }
 
     /**
@@ -14,42 +12,77 @@ public class Heap {
      * @param data value to be added.
      * @return True if the value had been added, false otherwise.
      */
-    public boolean add(int data){
-        if(data <= this.heap.get(0)){       // make sur the root is >= the others.
-            this.heap.add(data);
-            return true;
+    public void add(int data){
+        HeapNode n = new HeapNode(data);
+        if(this.heap.size() == 0 ){                     // set the root
+            this.heap.add(n);
         }
-        else{                               // if root is < the data, we have do to something.
-            //TODO Heapify
-            return false;
+        else{
+            this.heap.add(n);
+            correct(this,this.heap.size()-1);
         }
     }
 
     /**
-     * Gets the left child of the node at position 'i'.
-     * @param i Position of the parent node.
-     * @return Value of the left child.
+     * Recursive method used to correct a given Heap , starting with the node at the position 'index'.
+     * Note : This algorithm has a complexity of O(index) => O(N).
+     * @param A Heap to be corrected.
+     * @param index Position of the wrongfully placed HeapNode in the Heap A.
      */
-    public int getLeft(int i){
-        return this.heap.get(2*i);
+    private void correct(Heap A,int index){
+        HeapNode childNode = A.heap.get(index);
+        HeapNode fatherNode = A.getFather(childNode);
+        if (index == 0){                        // if index = 0 it means we're checking the root node. The root node is always >= the others.
+            return;
+        }
+        //check if child(index) > father:
+        else if (childNode.getValue() > fatherNode.getValue()) {
+            int fatherIndex = A.heap.indexOf(fatherNode);
+            A.heap.set(fatherIndex,childNode);          // A[fatherIndex] = childNode
+            A.heap.set(index,fatherNode);               // A[childIndex] = fatherNode
+            correct(A,fatherIndex);
+        }
+    }
+
+
+    /**
+     * Gets the left child of the node.
+     * @param hn The parent node.
+     * @return Left child Node.
+     */
+    public HeapNode getLeft(HeapNode hn){
+        var index = this.heap.indexOf(hn);
+        return this.heap.get(2*index);
     }
 
     /**
      * Gets the right child of the node at position 'i'.
-     * @param i Position of the parent node.
-     * @return Value of the right child.
+     * @param hn The parent node.
+     * @return The right child Node.
      */
-    public int getRight(int i){
-        return this.heap.get((2*i) + 1);
+    public HeapNode getRight(HeapNode hn){
+        var index = this.heap.indexOf(hn);
+        return this.heap.get((2*index) + 1);
     }
 
     /**
-     * Gets the father of the node at position 'i'.
-     * @param i Position of child.
-     * @return Value of the father.
+     * Gets the father of the node.
+     * @param hn The node child.
+     * @return The father Node.
      */
-    public int getFather(int i){
-        return this.heap.get(i/2);
+    public HeapNode getFather(HeapNode hn){
+        var index = this.heap.indexOf(hn);
+        return this.heap.get(index/2);
     }
 
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Heap{");
+        for(int i = 0; i < this.heap.size();i++){
+            stringBuilder.append(this.heap.get(i).getValue()+", ");
+        }
+        stringBuilder.append(" }");
+        return stringBuilder.toString();
+    }
 }
---
+++ src/main/java/HeapNode.java
@@ -0,0 +1,12 @@
+public class HeapNode {
+
+    private final int value;
+
+    public HeapNode(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
