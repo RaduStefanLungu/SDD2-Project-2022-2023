2567b8aeb44052029de945d3eda6706aaaf3a43c
Louis Rigaud <210943@umons.ac.be>
2023-03-12 12:25:03 +0100

    javadoc backend

--- src/main/java/BackEnd.java
+++ src/main/java/BackEnd.java
@@ -6,13 +6,32 @@ import java.util.ArrayList;
 import java.lang.Math;
 
 public class BackEnd {
+
+    /**
+   * La liste de segments verticaux qui sera triée en x
+   */
     private ArrayList<Segment> ver = new ArrayList<Segment>();
+    /**
+   * La liste de segments horizontaux qui sera triée en y
+   */
     private ArrayList<Segment> hor = new ArrayList<Segment>();
+    /**
+   * La liste de segments renvoyés en réponse à une requête
+   */
     private ArrayList<Segment> answer = new ArrayList<Segment>();
 
+    /**
+   * L'arbre de priorité des segments verticaux
+   */
     PriorityTree verTree = new PriorityTree();
+    /**
+   * L'arbre de priorité des segments horizontaux
+   */
     PriorityTree horTree = new PriorityTree();
 
+    /**
+   * Les bornes maximales
+   */
     private int low, up, left, rigth;
 
     // NOTE : POUR RUN CE MAIN-CI, FAIT UN <GRADLE RUN> (vu que le main de base est le MainFX
@@ -55,6 +74,10 @@ public class BackEnd {
     }
     */
 
+    /**
+     * Fonction principale pour l'initialisation des arbres de priorité
+     * @param file fichier texte avec les bornes maximales et les segments verticaux et horizontaux
+     */
     public void TreeCreate(File file){
         ver.clear();
         hor.clear();
@@ -65,6 +88,10 @@ public class BackEnd {
         horTree = Tree(hor);
     }
 
+    /**
+     * Lis et initialise les bornes maximales et trie les segments en deux tableaux
+     * @param file fichier texte avec les bornes maximales et les segments verticaux et horizontaux
+     */
     private void Read(File file){
         Scanner scanner;
         try {
@@ -94,6 +121,12 @@ public class BackEnd {
         }
     }
 
+    /**
+     * Trie un tableau de segment en fonction de la valeur constante
+     * @param list liste à trier
+     * @param start indice du premier élément
+     * @param end indice du dernier élément
+     */
     private void QuickSort(ArrayList<Segment> list, int start, int end){
          if (start<end){
             int j = Partition(list, start, end);
@@ -102,6 +135,14 @@ public class BackEnd {
          }
     }
 
+    /**
+     * Trouve l'indice qui sert à diviser le tableau en deux partie dont une plus petite que l'élément en question
+     * et l'autre plus grande
+     * @param list liste à trier
+     * @param start indice du premier élément
+     * @param end indice du dernier élément
+     * @return indice de l'élément qui divise le tableau
+     */
     private int Partition(ArrayList<Segment> list, int start, int end){
         int j = start;
         for (int i = start; i < end-1; i++){
@@ -118,6 +159,12 @@ public class BackEnd {
         return j;
     }
 
+    /**
+     * Crée un arbre de priorité en fonction de l'axe qui possède deux valeurs différentes
+     * et maintien un équilibre en fonction de la troisième valeur
+     * @param list liste triée selon la valeur constante du segment
+     * @return arbre de priorité
+     */
     private PriorityTree Tree(ArrayList<Segment> list){
         Segment min = FindMin(list);
         list.remove(list.indexOf(min));
@@ -150,6 +197,11 @@ public class BackEnd {
             }
     }
 
+    /**
+     * Renvoie le segment donc la valeur gauche ou basse (en fonction du tpe de segment) est la plus petite
+     * @param list liste triée selon la valeur constante du segment
+     * @return segment minimum
+     */
     private Segment FindMin(ArrayList<Segment> list){
         Segment min = list.get(0);
         for (int i=1; i<list.size(); i++){
@@ -160,11 +212,27 @@ public class BackEnd {
         return min;
     }
     
+    /**
+     * Fonction principale pour les requêtes de windowing
+     * @param x1 borne gauche
+     * @param x2 borne droite
+     * @param y1 borne basse
+     * @param y2 borne haute
+     */
     public void Query(int x1, int x2, int y1, int y2){
         answer.clear();
         QueryAnswer(verTree, y1, y2, x1, x2);
         QueryAnswer(horTree, x1, x2, y1, y2);
     }
+
+    /**
+     * Parcours un arbre de priorité et sélectionne les segments qui répondent à la requête
+     * @param tree arbre de priorité de segments horizontaux ou verticaux
+     * @param dif1 borne gauche pour les segments horizontaux et basse pour les segments verticaux
+     * @param dif2 borne droite pour les segments horizontaux et haute pour les segments verticaux
+     * @param cons1 borne basse pour les segments horizontaux et gauche pour les segments verticaux
+     * @param cons2 borne haute pour les segments horizontaux et droite pour les segments verticaux
+     */
     public void QueryAnswer(PriorityTree tree, int dif1, int dif2, int cons1, int cons2){
         if (tree.getRoot().getDif1()<=dif2){
             if(tree.getRoot().getDif2()>=dif1 && tree.getRoot().getCons()>=cons1 && tree.getRoot().getCons()<=cons2){
@@ -181,30 +249,58 @@ public class BackEnd {
         }
     }
 
+    /**
+     * Getteur pour la borne basse maximale
+     * @return la borne basse maximale
+     */
     public int getLow(){
         return low;
     }
 
+    /**
+     * Getteur pour la borne haute maximale
+     * @return la borne haute maximale
+     */
     public int getUp(){
         return up;
     }
 
+    /**
+     * Getteur pour la borne gauche maximale
+     * @return la borne gauche maximale
+     */
     public int getLeft(){
         return left;
     }
 
+    /**
+     * Getteur pour la borne droite maximale
+     * @return la borne droite maximale
+     */
     public int getRigth(){
         return rigth;
     }
 
+    /**
+     * Getteur pour l'arbre de priorité des segments verticaux
+     * @return l'arbre de priorité des segments verticaux
+     */
     public PriorityTree getVerTree(){
         return verTree;
     }
 
+    /**
+     * Getteur pour l'arbre de priorité des segments horizontaux
+     * @return l'arbre de priorité des segments horizontaux
+     */
     public PriorityTree getHorTree(){
         return horTree;
     }
 
+    /**
+     * Getteur pour le tableau de segments de réponse à la requête de windowing
+     * @return le tableau de segments de réponse à la requête de windowing
+     */
     public ArrayList<Segment> getAnswer(){
         return answer;
     }
