07eecc178fdb281504a819ad593a6fdddc9e857e
Louis Rigaud <210943@umons.ac.be>
2023-03-15 13:55:27 +0100

    javadoc segment et prioritytree

--- src/main/java/PriorityTree.java
+++ src/main/java/PriorityTree.java
@@ -2,12 +2,32 @@ import java.awt.*;
 
 public class PriorityTree {
 
+    /**
+     * Segment dans la racine de l'arbre
+     */
     private Segment root;
 
-    private int mid, type;
+    /**
+     * Entier qui sépare les coordonnées invariables restantes en deux
+     */
+    private int mid;
+    /**
+     * Le type permet de définir si l'arbre a un fils gauche et/ou un fils droit
+     */
+    private int type;
 
+    /**
+     * Arbres des fils gauche et droit
+     */
     private PriorityTree left, rigth;
     
+    /**
+     * Création d'un arbre de priorité qui a un fils gauche et un fils droit
+     * @param seg segment dans la racine
+     * @param m entier qui sépare les coordonnées invariables restantes en deux
+     * @param tree1 arbre gauche
+     * @param tree2 arbre droit
+     */
     public PriorityTree(Segment seg, int m, PriorityTree tree1, PriorityTree tree2){
         root = seg;
         mid = m;
@@ -16,6 +36,12 @@ public class PriorityTree {
         type = 0;
     }
 
+    /**
+     * Création d'un arbre de priorité qui n'a qu'un fils gauche
+     * @param seg segment dans la racine
+     * @param m entier plus grand ou égal à la coordonnée restante
+     * @param tree1 arbre gauche
+     */
     public PriorityTree(Segment seg, int m,PriorityTree tree1){
         root = seg;
         mid = m;
@@ -23,29 +49,56 @@ public class PriorityTree {
         type = 1;
     }
 
+    /**
+     * Création d'un arbre de priorité qui n'a pas de fils
+     * @param seg segment dans la racine
+     */
     public PriorityTree(Segment seg){
         root = seg;
         type = 2;
     }
 
+    /**
+     * Création d'un arbre vide pour l'initialisation du backend
+     */
     public PriorityTree(){}
 
+    /**
+     * Getteur pour le segment dans la racine
+     * @return le segment dans la racine
+     */
     public Segment getRoot(){
         return root;
     }
 
+    /**
+     * Getteur pour l'entier qui sépare les coordonnées invariables restantes en deux
+     * @return l'entier qui sépare les coordonnées invariables restantes en deux
+     */
     public int getMid(){
         return mid;
     }
 
+    /**
+     * Getteur pour l'arbre du fils gauche
+     * @return l'arbre du fils gauche
+     */
     public PriorityTree getLeft(){
         return left;
     }
 
+    /**
+     * Getteur pour l'arbre du fils droit
+     * @return l'arbre du fils droit
+     */
     public PriorityTree getRigth(){
         return rigth;
     }
 
+    /**
+     * Getteur pour le type d'arbre qui donne le nombre de fils
+     * @return le type d'arbre qui donne le nombre de fils
+     */
     public int getType(){
         return type;
     }
--- src/main/java/Segment.java
+++ src/main/java/Segment.java
@@ -2,26 +2,43 @@ import java.awt.*;
 
 public class Segment {
 
+    /**
+     * Coordonnées du segment
+     */
     private int dif1,dif2,cons;
 
+    /**
+     * Création d'un segment
+     * @param x première coordonnée qui varie dans son axe
+     * @param y deuxième coordonnée qui varie dans son axe
+     * @param z coordonnée invariable dans son axe
+     */
     public Segment(int x, int y, int z){
         dif1 = x;
         dif2 = y;
         cons = z;
     }
 
-    public int[] getCoord(){
-        return new int[] {this.dif1,this.dif2,this.cons};
-    }
-
+    /**
+     * Getteur pour la première coordonnée qui varie dans son axe
+     * @return la première coordonnée qui varie dans son axe
+     */
     public int getDif1(){
         return dif1;
     }
 
+    /**
+     * Getteur pour la deuxième coordonnée qui varie dans son axe
+     * @return la deuxième coordonnée qui varie dans son axe
+     */
     public int getDif2(){
         return dif2;
     }
 
+    /**
+     * Getteur pour la coordonnée invariable dans son axe
+     * @return la coordonnée invariable dans son axe
+     */
     public int getCons(){
         return cons;
     }
